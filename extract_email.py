1c5d582e50a1b52003bb034efab809a4362d858b
olivier <odelcroi@gmail.com>
2023-04-07 18:39:51 +0200

    add cron

--- extract_email.py
+++ extract_email.py
@@ -5,6 +5,14 @@ from base64 import b64encode
 from typing import Optional, Dict, List
 from dotenv import load_dotenv
 
+
+"""
+This script is meant to be run every minute or so by a cron job
+Its goal is to search for conversations in script which contains the rageshake@beta.gouv.fr 
+and to replace it by the real email of the participant if it can be found in the discussion fields
+"""
+
+
 # load environment variables from .env file
 load_dotenv()
 
@@ -12,15 +20,25 @@ CRISP_IDENTIFIER = os.environ["CRISP_IDENTIFIER"]
 CRISP_KEY = os.environ["CRISP_KEY"]
 CRISP_WEBSITE_ID = os.environ["CRISP_WEBSITE_ID"]
 DEFAULT_EMAIL = os.environ.get("DEFAULT_EMAIL", "rageshake@beta.gouv.fr")
+DEFAULT_EMAIL = os.environ.get("RAGESHAKE_NAME", "Tchap Rageshake")
 DRY_RUN = os.environ.get("DRY_RUN", "true").lower() == "true"
 
 EMAIL_REGEX = r"email: ?\s*\"([^\"]+)\""
-
+USER_ID_REGEX = r"user_id: ?\s*\"([^\"]+)\""
 
 def extract_email_from_message(message: str) -> Optional[str]:
+    if not isinstance(message, str):
+        return;
+
     email_match = re.search(EMAIL_REGEX, message)
+    #print(f"regex email match {email_match}")
     return email_match.group(1) if email_match else None
 
+def extract_user_id_from_message(message: str) -> Optional[str]:
+    if not isinstance(message, str):
+        return;
+    user_id_match = re.search(USER_ID_REGEX, message)
+    return user_id_match.group(1) if user_id_match else None
 
 def get_auth_headers():
     auth_string = f"{CRISP_IDENTIFIER}:{CRISP_KEY}"
@@ -30,11 +48,13 @@ def get_auth_headers():
         "X-Crisp-Tier": "plugin",
     }
 
-def get_conversations(page_number: int) -> List[Dict]:
+def get_invalid_conversations(page_number: int) -> List[Dict]:
+    #url = f"https://api.crisp.chat/v1/website/{CRISP_WEBSITE_ID}/conversations/"
     url = f"https://api.crisp.chat/v1/website/{CRISP_WEBSITE_ID}/conversations/{page_number}"
     headers = get_auth_headers()
     params = {
         "search_query": DEFAULT_EMAIL,
+        #"search_query" : "Tchap Rageshake", test to see what this query returns: It returns all conversations with the user Tchap Rageshake
         "search_type": "text",
     }
 
@@ -51,6 +71,7 @@ def get_messages(conversation_id: str) -> List[Dict]:
     return response.json()["data"]
 
 def update_email(conversation_id: str, email: str) -> None:
+    print(f"update {email} in {conversation_id}")
     update_url = f"https://api.crisp.chat/v1/website/{CRISP_WEBSITE_ID}/conversation/{conversation_id}/meta"
     update_payload = {
         "email": email
@@ -59,34 +80,60 @@ def update_email(conversation_id: str, email: str) -> None:
     response = requests.patch(update_url, headers=headers, json=update_payload)
     response.raise_for_status()
 
-def process_conversations(conversations: List[Dict]) -> bool:
-    for conversation in conversations:
-        conversation_id = conversation["session_id"]
+
+def process_conversation(conversation_id:str, verbose=False) -> bool:
         print(f"# Extract data from {conversation_id}")
         messages = get_messages(conversation_id)
-        first_message = messages[0]["content"]
+        #first_message = messages[0]["content"]
+        message_contents = list(map(lambda message: str(message["content"]), messages))  # Extract the "content" field from each message
+        combined_messages = "".join(message_contents).replace("\n","")  # Concatenate the message contents together into a single string
 
-        email = extract_email_from_message(first_message)
+        if verbose: 
+            print(f"all messages : {combined_messages}")
+
+
+
+        email = extract_email_from_message(combined_messages)
+        userId = extract_user_id_from_message(combined_messages)
+        print(f"found in {conversation_id}: userId: {userId}, email {email}")
 
         if email:
-            print(f"will change {email} to {conversation_id}")
             if not DRY_RUN:
                 update_email(conversation_id, email)
+                return True
+        return False
 
-    return False
+
+def process_conversations(conversations: List[Dict]) -> int:
+    total_updated_rageshake=0
+    for conversation in conversations:
+        if(process_conversation(conversation["session_id"])):
+            total_updated_rageshake+=1
+    
+    return total_updated_rageshake
 
 def main():
-    print("Start Extraction")
-    page_number = 1
+    print("**** Start Extraction ****")
+    page_number = 0
+    total_invalid_rageshake = 0
+    total_updated_rageshake = 0
     while True:
-        conversations = get_conversations(page_number)
+        conversations = get_invalid_conversations(page_number)
+        total_invalid_rageshake += len(conversations)
+        
+        print(f"In page {page_number}, # conversations with invalid rageshake : {len(conversations)}")
+
         if not conversations:
             break
 
-        stop_processing = process_conversations(conversations)
-        if stop_processing:
-            break
-    print("Finish Extraction")
+        total_updated_rageshake += process_conversations(conversations)
+        
+        page_number += 1
+
+    print("**** Finish Extraction ****")
+    print(f"Invalid rageshake found {total_invalid_rageshake}")
+    print(f"Invalid rageshake updated {total_updated_rageshake}")
+    print(f"Invalid rageshake remaining {total_invalid_rageshake-total_updated_rageshake}")
 
 if __name__ == "__main__":
     main()
--- test_extract_email.py
+++ test_extract_email.py
@@ -1,28 +1,17 @@
 import unittest
-from extract_email import user_id_to_email, extract_email_from_message, extract_user_id_from_message
+from extract_email import  extract_email_from_message, extract_user_id_from_message, process_conversation,get_invalid_conversations,get_messages
 
 class TestFunctions(unittest.TestCase):
-    def test_user_id_to_email(self):
-        user_id = '@julien.dauphant-beta.gouv.fr:agent.dinum.tchap.gouv.fr'
-        expected_email = 'julien.dauphant@beta.gouv.fr'
-        self.assertEqual(user_id_to_email(user_id), expected_email)
         
-        user_id = '@julien.dauphant-ac-paris.fr:agent.dinum.tchap.gouv.fr'
-        expected_email = 'julien.dauphant@ac-paris.fr'
-        self.assertEqual(user_id_to_email(user_id), expected_email)
-        
-        user_id = '@julien.dauphant-dgfip.gouv.fr:agent.dinum.tchap.gouv.fr'
-        expected_email = 'julien.dauphant@dgfip.gouv.fr'
-        self.assertEqual(user_id_to_email(user_id), expected_email)
-        
-        user_id = '@firstname.lastname1-2.3.4_5-example.com:agent.lol.tchap.gouv.f'
-        expected_email = 'firstname.lastname1-2.3.4_5@example.com'
-        self.assertEqual(user_id_to_email(user_id), expected_email)
-        
-        user_id = '@user.name+tag-example.com:agent.intradef.tchap.gouv.fr'
-        expected_email = 'user.name+tag@example.com'
-        self.assertEqual(user_id_to_email(user_id), expected_email)
-        
+
+    def test_conversation(self):
+        conversationId = "session_56c61974-7189-42ce-be8c-d697457463be"
+        #conversationId = "session_c28bb18b-45c5-420c-ab4f-3dd0a6da713f" #conversion found by email
+        #session_56c61974-7189-42ce-be8c-d697457463be/
+        conversation = process_conversation(conversationId , True)
+        print(f'conversion : {conversation}')
+
+
     def test_extract_email_from_message(self):
         message = 'email: "julien.dauphant@beta.gouv.fr"'
         expected_email = 'julien.dauphant@beta.gouv.fr'

b9e014aefc229f3e630439e137946504f2662b28
Olivier D <odelcroi@gmail.com>
2023-05-24 14:23:45 +0200

    extract email from userId for specific domains (#9)

--- test_extract_email.py
+++ test_extract_email.py
@@ -1,5 +1,5 @@
 import unittest
-from extract_email import  extract_email_from_message, extract_user_id_from_message, process_conversation,get_invalid_conversations,get_messages
+from extract_email import  extract_email_from_user_id, extract_email_from_message, extract_user_id_from_message, process_conversation,get_invalid_conversations,get_messages
 
 class TestFunctions(unittest.TestCase):
         
@@ -8,8 +8,8 @@ class TestFunctions(unittest.TestCase):
         conversationId = "session_56c61974-7189-42ce-be8c-d697457463be"
         #conversationId = "session_c28bb18b-45c5-420c-ab4f-3dd0a6da713f" #conversion found by email
         #session_56c61974-7189-42ce-be8c-d697457463be/
-        conversation = process_conversation(conversationId , True)
-        print(f'conversion : {conversation}')
+        conversation = process_conversation(conversationId , False)
+        #print(f'conversion : {conversation}')
 
 
     def test_extract_email_from_message(self):
@@ -36,5 +36,30 @@ class TestFunctions(unittest.TestCase):
         message = 'Please respond to firstname.lastname1-2.3.4_5@example.com'
         self.assertIsNone(extract_user_id_from_message(message))
         
+
+    def test_extract_email_from_user_id(self):
+        test_cases = [
+            ("@aaa.yyy-diplomatie.gouv.fr:agent.diplomatie.tchap.gouv.fr", 
+             "aaa.yyy@diplomatie.gouv.fr"),
+            ("@ooo.aaa-intradef.gouv.fr:agent.intradef.tchap.gouv.fr", 
+             "ooo.aaa@intradef.gouv.fr"),
+            ("@ppp-aaa.jjj-ap-hm.fr:agent.social.tchap.gouv.fr", 
+             "ppp-aaa.jjj@ap-hm.fr"),
+            ("@ppp-aaa.jjj-gendarmerie.interieur.gouv.fr:agent.interieur.tchap.gouv.fr", 
+             "ppp-aaa.jjj@gendarmerie.interieur.gouv.fr"),
+            ("@mmm.rrr-ac-aix-marseille.fr:agent.education.tchap.gouv.fr", 
+             "mmm.rrr@ac-aix-marseille.fr"),
+            ("@ppp.lll-ccc-douane.finances.gouv.fr:agent.finances.tchap.gouv.fr", 
+             "ppp.lll-ccc@douane.finances.gouv.fr"),
+            ("@fff.mmm-intradef.gouv.fr:agent.intradef.tchap.gouv.fr", 
+             "fff.mmm@intradef.gouv.fr")
+        ]
+
+        for user_id, expected_email in test_cases:
+            self.assertEqual(extract_email_from_user_id(user_id), expected_email)
+
+if __name__ == "__main__":
+    unittest.main()
+
 if __name__ == '__main__':
     unittest.main()

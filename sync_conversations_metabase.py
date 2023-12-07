49751cf0b1aa108a65089fe7a397cb88a4ade3b8
Raphaël ALLOUCHE <raphael.allouche@beta.gouv.fr>
2023-12-07 15:03:28 +0100

    pg connection method

--- sync_conversations_metabase.py
+++ sync_conversations_metabase.py
@@ -1,7 +1,8 @@
 import requests
-
+import os
+import psycopg2
 from typing import  Dict, List
-
+from dotenv import load_dotenv
 from utils import get_auth_headers
 
 
@@ -18,3 +19,27 @@ def get_conversations(page_number: int) -> List[Dict]:
 
 def job_sync_conversations_metabase(conversations_max:int = 0):
     #get last 200 conversations
+    # push each conversation to postgres
+
+
+def metabase_database_connect():
+    load_dotenv()
+    METABASE_PG_ENDPOINT = os.environ[""]
+    METABASE_PG_DATABASE = os.environ[""]
+    METABASE_PG_PORT     = os.environ[""]
+    METABASE_PG_USER     = os.environ[""]
+    METABASE_PG_PASS     = os.environ[""]
+    pg_connection_dict = {
+        'dbname':   METABASE_PG_DATABASE,
+        'user':     METABASE_PG_USER,
+        'password': METABASE_PG_PASS,
+        'port':     METABASE_PG_PORT,
+        'host':     METABASE_PG_ENDPOINT
+    }
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**pg_connection_dict)
+        return conn
+    except (Exception, psycopg2.DatabaseError) as error:
+        return error

2cb8e9cfdc9ed5bd95909b1210b7907128c5e398
olivier <odelcroi@gmail.com>
2023-12-08 14:21:12 +0100

    add job to cron

--- start_cron.py
+++ start_cron.py
@@ -4,7 +4,7 @@ import os
 
 from extract_email import job_process_invalid_rageshake
 from sleepy_conversations import job_process_sleepy_conversations
-
+from job_export_crips_conversation_segments_s3 import job_export_crips_conversation_segments_s3
 from ConversationIdStorage import ConversationIdStorage
 
 #this script is started by the container
@@ -16,11 +16,17 @@ processConversationIds = ConversationIdStorage()
 def start_cron():
     cron_shedule:str = os.environ["SCHEDULE_CRISP_INVALID_RAGESHAKE"]
     cron_sleepy_conversation:str = os.environ["SCHEDULE_JOB_SLEEPY_CONVERSATION_IN_HOURS"]
+    cron_export_segments_to_stat:str = os.environ["SCHEDULE_JOB_EXPORT_SEGMENTS_IN_DAYS"]
 
+    # every 'cron_shedule' seconds process new conversations
     schedule.every(int(cron_shedule)).seconds.do(job_process_invalid_rageshake, processConversationIds=processConversationIds)
     
-    #process 10 sleepy conversations
+    # process 10 sleepy conversations every 'cron_sleepy_conversation' hours
     schedule.every(int(cron_sleepy_conversation)).hours.do(job_process_sleepy_conversations, 10)
+
+    # every 'cron_export_segments_to_stat' days export the conversations from last 10 days
+    schedule.every(int(cron_export_segments_to_stat)).days.do(job_export_crips_conversation_segments_s3, 10)
+
 
     while True:
         schedule.run_pending()
